db54f27c6f4eacaaac556b7ccb384385eda27afa
Pierre Schubert <366256@gmail.com>
2013-02-19 15:44:48 +0100

    Completed example

--- example/App.js
+++ example/App.js
@@ -1,8 +1,14 @@
 var express = require("express"),
-    app = express();
+    app = express(),
+    TranslateMe = require("../lib/TranslateMe"),
+    translateMe = new TranslateMe(process.env.MONGO_URL, "en", ["de", "fr"]);
+
 
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+
+// It is imported to place the reqisterRoutes between after all use statements
+translateMe.registerRoutes(app, true);
 
 app.get("/", function(req, res){
     var body = "Hello World";
