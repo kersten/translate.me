48a5e8b1b2ac34e89ec2bf06f9312f7c7b8150c9
Kersten Burkhardt <kerstenk@gmail.com>
2013-02-19 11:37:44 +0100

    initial example app

--- example/App.js
+++ example/App.js
@@ -1,1 +1,15 @@
+var express = require("express"),
+    app = express();
 
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+app.get("/", function(req, res){
+    var body = "Hello World";
+    res.setHeader("Content-Type", "text/plain");
+    res.setHeader("Content-Length", body.length);
+    res.end(body);
+});
+
+app.listen(3000);
+console.log("Listening on port 3000");
