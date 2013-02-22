c17c89649ebab1af1c8622d44413b3de8c7ee0d7
Pierre Schubert <366256@gmail.com>
2013-02-22 10:44:13 +0100

    Added create on failing translate and updated TranslateMe construction
    - Changed method of defining the getLocale function when constructing
    TranslateMe. You now have to override the getLocale function from the
    TranslateMe object, if you want an custom behaviour.
    - Changed the default behaviour of getLocale to return the best
    language based on the reuests header "accpeted-languages"
    - TranslateMe now registers the locale middleware by itself
    - Enhanced "/i18n/translations" (post) to check for required parameters
    and respond accordingly. Furthermore enabled creation of new
    translation without a locale. In this case the default locale will be
    used.
    - Added support for requesting translations based on multiple locales
    in "/i18n/translations" (get). Furthermore you can now retrieve all
    translations, by passing no locale parameter.
    - Enhanced stripping of "ns:" placeholder in underscore.translate

--- example/App.js
+++ example/App.js
@@ -1,19 +1,30 @@
 var express = require("express"),
     app = express(),
-    TranslateMe = require("../lib/TranslateMe"),
-    translateMe = new TranslateMe(process.env.MONGO_URL, "en", ["de", "fr"]);
-
+    TranslateMe = require("../lib/TranslateMe");
 
 app.use(express.bodyParser());
 app.use(express.methodOverride());
-
-/*
-It is imported to place the "reqister routers" after
-all use-statements, to prevent any blockage of other middlewares.
-*/
-translateMe.registerRoutes(app, true);
 app.use("/js/underscore", express.static(__dirname + "/../node_modules/underscore/"));
 app.use("/translate.me", express.static(__dirname + "/../lib/browser"));
+
+/*
+ * Create a new TranslateMe object which connects to "", has a default locale of "en"
+ * and furthermore supports the locales "de" and "fr".
+ */
+translateMe = new TranslateMe(process.env.MONGO_URL, "en", ["de", "fr"]);
+
+/*
+ * Overwrite getLocale function, to enable custom selection of the current locale.
+ */
+translateMe.getLocale = function(req, callback) {
+    callback("de");
+}
+
+/*
+ * It is imported to place the "reqister routers" after
+ * all use-statements, to prevent any blockage of other middlewares.
+ */
+translateMe.registerRoutes(app, true);
 
 app.get("/", function(req, res){
     res.sendfile(__dirname + "/views/index.html");
