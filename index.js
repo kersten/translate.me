f07c4dec21bd632e0392360b56ea943a99de3f09
Pierre Schubert <366256@gmail.com>
2013-04-15 12:32:59 +0200

    Introduces server-side translation support
    Rearranged directories in lib direcotry to follow server, browser and
    shared modules.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-module.exports = require("./lib/TranslateMe");
+var TranslateMe = new require("./lib/server/TranslateMe"),
+    translateMe;
+
+module.exports = function(options) {
+    if(!translateMe && options) {
+        translateMe = new TranslateMe(options.mongoURL, options.defaultLocale, options.supportedLocales);
+    } else if(!translateMe && !options) {
+        throw new Error("Please pass an options object to initialize translate.me");
+    } else if(translateMe && options) {
+        throw new Error("Please do not pass options! Translate.me has been initialized before.");
+    }
+    return translateMe;
+}
