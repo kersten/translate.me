d63fc607a15da1a63f2de3b94282abb84c252f8c
Pierre Schubert <366256@gmail.com>
2013-07-16 09:44:59 +0200

    New version 1.0.0-5

--- translate.me.js
+++ translate.me.js
@@ -1,4 +1,4 @@
-define(['./lib/public/ajax-translations-handler.amd.js', './lib/handlebars-helpers.umd.js', './lib/translator.umd.js'],
+define(['lib/public/ajax-translations-handler.amd', 'lib/handlebars-helpers.umd', 'lib/translator.umd'],
     function(AjaxTranslationHandler, Helpers, Translator) {
     'use strict';
 
